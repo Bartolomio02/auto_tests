29d20c90839da3170615ec24993b513c452f2902
Matvey <bartolomio2.02@gmail.com>
2022-04-14 18:01:49 +0300

    Добавление примера selenium webdriverwait

---
+++ selenium_practik_webdriverwait.py
@@ -0,0 +1,43 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import math
+import os
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    price = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), '$100'))
+    if price == True:
+        btn = browser.find_element(By.ID, "book")
+        btn.click()
+
+    num = browser.find_element(By.ID, "input_value")
+    reply = calc(num.text)
+
+    text_area = browser.find_element(By.CLASS_NAME, 'form-control')
+    text_area.send_keys(reply)
+
+    btn_submit = browser.find_element(By.ID, 'solve')
+    btn_submit.click()
+
+    alert = browser.switch_to.alert
+    print(alert.text.split(' ')[-1])
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(2)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
